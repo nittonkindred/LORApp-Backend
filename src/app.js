1668d1fc7d264136dc703d471860db3c16ee5237
Kiều Trung Tín <mantinited@gmail.com>
2026-07-22 23:08:59 +0700

    Create barebone project structure

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import routes from './routes/index.js';
+import express from "express";
+import routes from "./routes/index.js";
 
 const app = express();
 
 app.use(express.json());
-app.use('/api', routes);
+app.use("/api", routes);
 
 export default app;
